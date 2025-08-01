4fec895a1c7c5c5981cdf2ba30877bf4be00783f
ltyohannes955 <ltyohannes95@gmail.com>
2025-08-01 17:52:06 +0300

    done with capaciter set up

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from "@capacitor/cli";
+
+const config: CapacitorConfig = {
+  appId: "com.BlockBuzz.tito",
+  appName: "BlockBuzz",
+  webDir: "dist",
+};
+
+export default config;
