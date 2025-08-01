500e76df6932b507ed6aaf3f1483a24ca474662d
ltyohannes955 <ltyohannes95@gmail.com>
2025-08-01 17:36:19 +0300

    done with mantine core set up and as well as notification set up

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "@mantine/core/styles.css";
+import "@mantine/notifications/styles.css";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+import { MantineProvider } from "@mantine/core";
+import { Notifications } from "@mantine/notifications";
+
+createRoot(document.getElementById("root")!).render(
+  <MantineProvider>
+    <Notifications />
     <App />
-  </StrictMode>,
-)
+  </MantineProvider>
+);
