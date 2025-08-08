aebcad9c39f2f41fcf40f1e1a8624aa939116761
Leul Yohannes <106162490+ltyohannes955@users.noreply.github.com>
2025-08-08 12:49:26 +0300

    added capacitor/app and configered app.tsx to handel back button on phones

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,37 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  useNavigate,
+  useLocation,
+} from "react-router-dom";
+import { useEffect } from "react";
+import { App as CapApp } from "@capacitor/app";
 import Home from "./pages/home";
 import MyEvents from "./pages/myEvents";
 import Landing from "./pages/landing";
 import Onbording from "./pages/onbording";
-import Signup from "./pages/signup";
+import Signup from "./pages/auth/signup";
+import Login from "./pages/auth/login";
 
 function App() {
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  useEffect(() => {
+    const handler = CapApp.addListener("backButton", () => {
+      // Prevent app exit on root, otherwise go back
+      if (location.pathname === "/" || location.pathname === "/home") {
+        CapApp.exitApp();
+      } else {
+        navigate(-1); // Go back one page
+      }
+    });
+
+    return () => {
+      handler.then((h) => h.remove());
+    };
+  }, [location, navigate]);
   return (
     <BrowserRouter>
       <Routes>
@@ -14,6 +40,7 @@ function App() {
         <Route path="/home" element={<Home />} />
         <Route path="/myEvents" element={<MyEvents />} />
         <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </BrowserRouter>
   );
