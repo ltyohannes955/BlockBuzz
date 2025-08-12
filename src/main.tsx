bfd018fe00bb21dd2ee5ccd7ab2ed62c582581e6
Leul Yohannes <106162490+ltyohannes955@users.noreply.github.com>
2025-08-12 13:14:02 +0300

    added carousel styles for all pages

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "@mantine/core/styles.css";
 import "@mantine/notifications/styles.css";
+import "@mantine/carousel/styles.css";
 import { BrowserRouter } from "react-router-dom";
 
 import { MantineProvider } from "@mantine/core";
